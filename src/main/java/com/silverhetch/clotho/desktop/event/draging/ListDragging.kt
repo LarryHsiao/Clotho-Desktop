d8393b76822a67e2f1a3516ba3f61520ffe4be55
Larry Hsiao <larryhsiao@silverhetch.com>
2019-03-02 12:43:38 +0800

    ListDragging

--- src/main/java/com/silverhetch/clotho/desktop/event/draging/ListDragging.kt
+++ src/main/java/com/silverhetch/clotho/desktop/event/draging/ListDragging.kt
@@ -4,16 +4,19 @@ import javafx.event.EventHandler
 import javafx.scene.control.ListCell
 import javafx.scene.control.ListView
 import javafx.scene.input.DragEvent
+import javafx.scene.input.DragEvent.DRAG_ENTERED
 
 class ListDragging<T>(
     private val cell: ListCell<T>,
     private val listView: ListView<T>
 ) : EventHandler<DragEvent> {
-    override fun handle(it: DragEvent?) {
-        it?.let {
-            listView.selectionModel.clearSelection()
-            listView.selectionModel.select(cell.item)
-            it.consume()
+    override fun handle(it: DragEvent) {
+        when (it.eventType) {
+            DRAG_ENTERED -> {
+                listView.selectionModel.clearSelection()
+                listView.selectionModel.select(cell.item)
+                it.consume()
+            }
         }
     }
 }
